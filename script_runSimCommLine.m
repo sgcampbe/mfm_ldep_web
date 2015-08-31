da4b9e255ddbd6a1f88ccd733fb948a8b918d617
admin <admin@icbl-server-admin.local>
2015-08-31 11:31:04 -0400

    Added filename parameter

--- script_runSimCommLine.m
+++ script_runSimCommLine.m
@@ -1,8 +1,10 @@
+function script_runSimCommLine(fileName)
 disp('Running dummy script...')
 
-addpath('/User/admin/MATLAB_files/common/')
-
-func_simpleUnloadedTwitch
+addpath('/Users/admin/MATLAB_files/common/')
+savepath;
+cd /Users/admin/mfm_ldep_web;
+func_simpleUnloadedTwitch(fileName);
 
 exit
 
